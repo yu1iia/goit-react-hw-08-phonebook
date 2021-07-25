9fdc54b35dee81ab7237546ce44e708c0f84e612
Yuliia Burko <yuliia.burko@gmail.com>
2021-07-26 01:34:22 +0200

    Updates

---
+++ precache-manifest.efc83362810c3adae44503881de5926d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21348d613ee7c5613e99d887690f2354",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "66e737fb504bf5d397c5",
+    "url": "/goit-react-hw-08-phonebook/static/css/3.56db8995.chunk.css"
+  },
+  {
+    "revision": "632859e844fdaa42b952",
+    "url": "/goit-react-hw-08-phonebook/static/css/4.4339aaba.chunk.css"
+  },
+  {
+    "revision": "dcd1a5de53e371add7ab",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.c6b81991.chunk.css"
+  },
+  {
+    "revision": "9943f8df6b1da321d718",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.dcdc3017.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.dcdc3017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e737fb504bf5d397c5",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.037b9721.chunk.js"
+  },
+  {
+    "revision": "632859e844fdaa42b952",
+    "url": "/goit-react-hw-08-phonebook/static/js/4.9d7b4016.chunk.js"
+  },
+  {
+    "revision": "9bdfde9b91bdc10b6eef",
+    "url": "/goit-react-hw-08-phonebook/static/js/5.36624737.chunk.js"
+  },
+  {
+    "revision": "134100d3179dee372c4e",
+    "url": "/goit-react-hw-08-phonebook/static/js/6.d84a5ec4.chunk.js"
+  },
+  {
+    "revision": "f633a62c8ea9f5b9f27f",
+    "url": "/goit-react-hw-08-phonebook/static/js/7.27c5cc09.chunk.js"
+  },
+  {
+    "revision": "dcd1a5de53e371add7ab",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.89db4f0d.chunk.js"
+  },
+  {
+    "revision": "772059e61092c3a652f8",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.9b6c9970.js"
+  },
+  {
+    "revision": "1c3f4ef779330cc2557ba3835416269b",
+    "url": "/goit-react-hw-08-phonebook/static/media/phonebook.1c3f4ef7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-08-phonebook/precache-manifest.471d402c9e9d02761af2d05a10ec2b80.js"
+  "/goit-react-hw-08-phonebook/precache-manifest.efc83362810c3adae44503881de5926d.js"
 );
 
 self.addEventListener('message', (event) => {
